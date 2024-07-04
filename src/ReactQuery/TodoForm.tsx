b478af765d19731c641bed61cd439b59a58267c1
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-07-04 16:59:53 +0700

    Show Mutation progress

--- src/ReactQuery/TodoForm.tsx
+++ src/ReactQuery/TodoForm.tsx
@@ -19,6 +19,10 @@ const TodoForm = () => {
         savedTodo,
         ...(todos || []),
       ]);
+
+      if (ref.current) {
+        ref.current.value = "";
+      }
     },
   });
 
@@ -46,8 +50,10 @@ const TodoForm = () => {
         />
       </div>
       <div>
-        <button className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2 ml-2">
-          Add
+        <button
+          disabled={addTodo.isPending}
+          className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2 ml-2">
+          {addTodo.isPending ? "Adding ..." : "Add"}
         </button>
       </div>
       <Toaster richColors position="top-center" />
